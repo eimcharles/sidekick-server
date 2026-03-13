0ffbd611f528618855739eeb137757319996ebe4
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-03-13 10:22:49 -0400

    feat(user): add User model.

--- src/main/java/com/eimc/user/model/User.java
+++ src/main/java/com/eimc/user/model/User.java
@@ -1,4 +1,78 @@
 package com.eimc.user.model;
 
+import java.util.Objects;
+import java.util.UUID;
+
 public class User {
+
+    private UUID userId;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public User(UUID userId, String firstName, String lastName, String email, String password) {
+        this.userId = userId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(userId, user.userId) &&
+                Objects.equals(firstName, user.firstName) &&
+                Objects.equals(lastName, user.lastName) &&
+                Objects.equals(email, user.email) &&
+                Objects.equals(password, user.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(userId, firstName, lastName, email, password);
+    }
+
 }
