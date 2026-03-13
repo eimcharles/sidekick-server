7666d38862ba4d4d72e5d75864841348cb4d2675
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-03-13 12:45:02 -0400

    refactor(user): refactor controller from user to employee.

---
+++ src/main/java/com/eimc/employee/controller/EmployeeController.java
@@ -0,0 +1,14 @@
+package com.eimc.employee.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * */
+
+@RestController
+@RequestMapping("api/v1/employees")
+public class EmployeeController {
+
+}
