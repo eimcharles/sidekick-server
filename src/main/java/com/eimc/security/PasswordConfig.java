51c896c04aba1f96b83e86457eb8c22c64bbd854
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-03-13 10:48:10 -0400

    feat(security): implement BCrypt password encoding.

--- src/main/java/com/eimc/security/PasswordConfig.java
+++ src/main/java/com/eimc/security/PasswordConfig.java
@@ -1,4 +1,33 @@
 package com.eimc.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+/**
+ *      PasswordConfig defines
+ *      the password encoding strategy
+ *      using Bcrypt.
+ * */
+
+@Configuration
 public class PasswordConfig {
+
+    /**
+     *      BCryptPasswordEncoder is an
+     *      implementation of the
+     *      PasswordEncoder interface.
+     *
+     *      Setting the strength to
+     *      10 makes is computationally
+     *      expensive to break, yet
+     *      fast for user verification.
+     *
+     * */
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
 }
