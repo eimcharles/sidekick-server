34424fcffce1f5329d868d978a568ddc29d7b16a
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-03-23 15:47:32 -0400

    feat(controller): add secured GET endpoint for employee by UUID.

--- src/main/java/com/eimc/employee/controller/EmployeeController.java
+++ src/main/java/com/eimc/employee/controller/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.eimc.employee.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.eimc.employee.model.Employee;
+import com.eimc.employee.service.EmployeeService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
 
 /**
  *
@@ -10,5 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/employees")
 public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/{employeeId}")
+    @PreAuthorize("hasAuthority('employee:read')")
+    public ResponseEntity<?> getEmployeeByEmployeeId(@PathVariable UUID employeeId){
+        Employee employee = employeeService.getEmployeeByEmployeeId(employeeId);
+        return ResponseEntity.ok(employee);
+    }
 
 }
