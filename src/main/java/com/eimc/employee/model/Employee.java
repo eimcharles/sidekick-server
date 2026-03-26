9e15df9de8dca95c9fe07543455efc870b0c3b7a
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-03-26 16:14:14 -0400

    refactor(model): convert Employee class to JPA Entity.

--- src/main/java/com/eimc/employee/model/Employee.java
+++ src/main/java/com/eimc/employee/model/Employee.java
@@ -1,24 +1,75 @@
 package com.eimc.employee.model;
 
+import jakarta.persistence.*;
+
 import java.util.Objects;
 import java.util.UUID;
 
+@Entity
+@Table(name = "employees")
 public class Employee {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
     private UUID employeeId;
+
+    @Column(nullable = false)
     private String employeePosition;
+
+    @Column(nullable = false)
     private String firstName;
+
+    @Column(nullable = false)
     private String lastName;
+
+    @Column(nullable = false, unique = true)
     private String email;
+
+    @Column(nullable = false)
     private String password;
 
-    public Employee(UUID employeeId, String employeePosition, String firstName, String lastName, String email, String password) {
+    public Employee() {}
+
+    public Employee(Long id,
+                    UUID employeeId,
+                    String employeePosition,
+                    String firstName,
+                    String lastName,
+                    String email,
+                    String password) {
+        this.id = id;
         this.employeeId = employeeId;
         this.employeePosition = employeePosition;
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
         this.password = password;
+    }
+
+    public Employee(String employeePosition,
+                    String firstName,
+                    String lastName,
+                    String email,
+                    String password) {
+        this.employeePosition = employeePosition;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    @PrePersist
+    public void initializeUUID() {
+        if (this.employeeId == null) {
+            this.employeeId = UUID.randomUUID();
+        }
+    }
+
+    public Long getId() {
+        return id;
     }
 
     public UUID getEmployeeId() {
@@ -43,6 +94,10 @@ public class Employee {
 
     public String getPassword() {
         return password;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public void setEmployeeId(UUID employeeId) {
@@ -73,17 +128,18 @@ public class Employee {
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Employee employee = (Employee) o;
-        return Objects.equals(employeeId, employee.employeeId)
-                && Objects.equals(employeePosition, employee.employeePosition)
-                && Objects.equals(firstName, employee.firstName)
-                && Objects.equals(lastName, employee.lastName)
-                && Objects.equals(email, employee.email)
-                && Objects.equals(password, employee.password);
+        return Objects.equals(id, employee.id) &&
+                Objects.equals(employeeId, employee.employeeId) &&
+                Objects.equals(employeePosition, employee.employeePosition) &&
+                Objects.equals(firstName, employee.firstName) &&
+                Objects.equals(lastName, employee.lastName) &&
+                Objects.equals(email, employee.email) &&
+                Objects.equals(password, employee.password);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(employeeId, employeePosition, firstName, lastName, email, password);
+        return Objects.hash(id, employeeId, employeePosition, firstName, lastName, email, password);
     }
 
 }
