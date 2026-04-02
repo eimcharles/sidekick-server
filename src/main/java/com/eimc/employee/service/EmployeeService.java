446fe0352b5b1cff7d74846713d0271b0e4399ba
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-04-02 16:51:47 -0400

    feat(employee): integrate ApplicationUser creation into the EmployeeService.

--- src/main/java/com/eimc/employee/service/EmployeeService.java
+++ src/main/java/com/eimc/employee/service/EmployeeService.java
@@ -1,23 +1,30 @@
 package com.eimc.employee.service;
 
+import com.eimc.auth.ApplicationUser;
 import com.eimc.employee.model.Employee;
 import com.eimc.employee.repository.EmployeeRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Set;
 import java.util.UUID;
 
 @Service
 public class EmployeeService {
 
     private final EmployeeRepository employeeRepository;
+    private final PasswordEncoder passwordEncoder;
 
-    public EmployeeService(EmployeeRepository employeeRepository) {
+    public EmployeeService(EmployeeRepository employeeRepository, PasswordEncoder passwordEncoder) {
         this.employeeRepository = employeeRepository;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    public Employee createEmployee(Employee employee) {
+    @Transactional
+    public Employee createEmployee(Employee employee, String password, Set<String> grantedAuthorities) {
 
         Employee createdEmployee = new Employee(
                 employee.getEmployeePosition(),
@@ -26,6 +33,13 @@ public class EmployeeService {
                 employee.getEmail()
         );
 
+        ApplicationUser createdAccount = new ApplicationUser(
+                createdEmployee,
+                passwordEncoder.encode(password),
+                grantedAuthorities
+        );
+
+        createdEmployee.setApplicationUser(createdAccount);
         employeeRepository.save(createdEmployee);
         return createdEmployee;
     }
