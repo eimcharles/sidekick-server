37a32560525a24e7f0ec8709a14a303528beaa73
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-04-10 14:34:18 -0400

    feature(employee): standardize endpoint responses in EmployeeProfileController and change routing for updatePassword endpoint.

--- src/main/java/com/eimc/employee/controller/EmployeeProfileController.java
+++ src/main/java/com/eimc/employee/controller/EmployeeProfileController.java
@@ -1,12 +1,18 @@
 package com.eimc.employee.controller;
 
+import com.eimc.common.domain.HttpResponse;
 import com.eimc.employee.dto.request.PasswordUpdateRequest;
 import com.eimc.employee.dto.response.EmployeeResponse;
 import com.eimc.employee.model.Employee;
 import com.eimc.employee.service.EmployeeService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.Instant;
+import java.util.Map;
 
 /**
  *      EmployeeProfileController provides self-service endpoints
@@ -24,24 +30,44 @@ public class EmployeeProfileController {
     }
 
     @GetMapping
-    public ResponseEntity<EmployeeResponse> getEmployeeProfile(Authentication authentication){
+    public ResponseEntity<HttpResponse> getEmployeeProfile(
+            Authentication authentication,
+            HttpServletRequest request){
 
         String employeeUsername = authentication.getName();
         Employee employee = employeeService.getEmployeeByEmail(employeeUsername);
 
-        return ResponseEntity.ok(EmployeeResponse.mapToResponse(employee));
+        return ResponseEntity.ok().body(HttpResponse.builder()
+                        .timeStamp(Instant.now())
+                        .status(HttpStatus.OK)
+                        .statusCode(HttpStatus.OK.value())
+                        .path(request.getRequestURI())
+                        .requestMethod(request.getMethod())
+                        .message("Profile retrieved successfully")
+                        .data(Map.of("Employee", EmployeeResponse.mapToResponse(employee)))
+                        .build());
     }
 
-    @PatchMapping
-    public ResponseEntity<EmployeeResponse> updatePassword(
+    @PatchMapping("/update-password")
+    public ResponseEntity<HttpResponse> updatePassword(
             Authentication authentication,
-            @RequestBody PasswordUpdateRequest request){
+            HttpServletRequest request,
+            @RequestBody PasswordUpdateRequest passwordUpdateRequest){
 
         String employeeUsername = authentication.getName();
         employeeService.updatePassword(employeeUsername,
-                request.oldPassword(), request.newPassword(), request.newPasswordConfirmed());
+                passwordUpdateRequest.oldPassword(),
+                passwordUpdateRequest.newPassword(),
+                passwordUpdateRequest.newPasswordConfirmed());
 
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().body(HttpResponse.builder()
+                .timeStamp(Instant.now())
+                .status(HttpStatus.OK)
+                .statusCode(HttpStatus.OK.value())
+                .path(request.getRequestURI())
+                .requestMethod(request.getMethod())
+                .message("Password updated successfully")
+                .build());
     }
 
 }
