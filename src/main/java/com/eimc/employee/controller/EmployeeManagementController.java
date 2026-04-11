3dba467d9033bf2cc70f34f03fb8e6715d95c1ab
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-04-11 12:28:09 -0400

    feature(employee): standardize endpoint responses in EmployeeManagementController.

--- src/main/java/com/eimc/employee/controller/EmployeeManagementController.java
+++ src/main/java/com/eimc/employee/controller/EmployeeManagementController.java
@@ -1,17 +1,23 @@
 package com.eimc.employee.controller;
 
+import com.eimc.common.domain.HttpResponse;
 import com.eimc.employee.dto.request.EmployeeCreationRequest;
 import com.eimc.employee.dto.request.EmployeeUpdateRequest;
 import com.eimc.employee.dto.response.EmployeeAdminResponse;
 import com.eimc.employee.model.Employee;
 import com.eimc.employee.service.EmployeeService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
+import java.time.Instant;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 /**
  *      EmployeeManagementController provides administrative
@@ -30,60 +36,123 @@ public class EmployeeManagementController {
 
     @PostMapping
     @PreAuthorize("hasAuthority('employee:write')")
-    public ResponseEntity<EmployeeAdminResponse> createEmployee(@RequestBody EmployeeCreationRequest request){
+    public ResponseEntity<HttpResponse> createEmployee(
+            @RequestBody EmployeeCreationRequest employeeCreationRequest,
+            HttpServletRequest request){
 
         Employee newEmployee = new Employee(
-                request.employeePosition(),
-                request.firstName(),
-                request.lastName(),
-                request.email());
+                employeeCreationRequest.employeePosition(),
+                employeeCreationRequest.firstName(),
+                employeeCreationRequest.lastName(),
+                employeeCreationRequest.email());
 
         Employee createdEmployee = employeeService
-                .createEmployee(newEmployee, request.password(), request.role());
+                .createEmployee(newEmployee,
+                        employeeCreationRequest.password(),
+                        employeeCreationRequest.role());
 
-        return ResponseEntity.ok(EmployeeAdminResponse.mapToResponse(createdEmployee));
+        URI resourceLocation = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(createdEmployee.getEmployeeId())
+                .toUri();
+
+        return ResponseEntity.created(resourceLocation)
+                .body(HttpResponse.builder()
+                .timeStamp(Instant.now())
+                .statusCode(HttpStatus.CREATED.value())
+                .status(HttpStatus.CREATED)
+                .message("Employee successfully created")
+                .path(resourceLocation.getPath())
+                .requestMethod(request.getMethod())
+                .data(Map.of("Employee", EmployeeAdminResponse
+                        .mapToResponse(createdEmployee)))
+                .build());
+
     }
 
     @GetMapping("/{employeeId}")
     @PreAuthorize("hasAuthority('employee:read')")
-    public ResponseEntity<EmployeeAdminResponse> getEmployeeByEmployeeId(@PathVariable UUID employeeId){
+    public ResponseEntity<HttpResponse> getEmployeeByEmployeeId(
+            @PathVariable UUID employeeId,
+            HttpServletRequest request){
 
         Employee employee = employeeService.getEmployeeByEmployeeId(employeeId);
-        return ResponseEntity.ok(EmployeeAdminResponse.mapToResponse(employee));
+        return ResponseEntity.ok().body(HttpResponse.builder()
+                .timeStamp(Instant.now())
+                .statusCode(HttpStatus.OK.value())
+                .status(HttpStatus.OK)
+                .message("Employee successfully retrieved")
+                .path(request.getRequestURI())
+                .requestMethod(request.getMethod())
+                .data(Map.of("Employee", EmployeeAdminResponse
+                        .mapToResponse(employee)))
+                .build());
+
     }
 
     @PatchMapping("/{employeeId}")
     @PreAuthorize("hasAuthority('employee:update')")
-    public ResponseEntity<EmployeeAdminResponse> updateEmployee(
+    public ResponseEntity<HttpResponse> updateEmployee(
             @PathVariable UUID employeeId,
-            @RequestBody EmployeeUpdateRequest request) {
+            @RequestBody EmployeeUpdateRequest employeeUpdateRequest,
+            HttpServletRequest request) {
 
         Employee updatedEmployee = employeeService
-                .updateEmployee(employeeId, request.employeePosition(), request.firstName(), request.lastName());
+                .updateEmployee(employeeId,
+                        employeeUpdateRequest.employeePosition(),
+                        employeeUpdateRequest.firstName(),
+                        employeeUpdateRequest.lastName());
 
-        return ResponseEntity.ok(EmployeeAdminResponse.mapToResponse(updatedEmployee));
+        return ResponseEntity.ok().body(HttpResponse.builder()
+                .timeStamp(Instant.now())
+                .statusCode(HttpStatus.OK.value())
+                .status(HttpStatus.OK)
+                .message("Employee successfully updated")
+                .path(request.getRequestURI())
+                .requestMethod(request.getMethod())
+                .data(Map.of("Employee", EmployeeAdminResponse
+                        .mapToResponse(updatedEmployee)))
+                .build());
+
     }
 
     @DeleteMapping("/{employeeId}")
     @PreAuthorize("hasAuthority('employee:delete')")
-    public ResponseEntity<EmployeeAdminResponse> deleteEmployee(@PathVariable UUID employeeId){
+    public ResponseEntity<HttpResponse> deleteEmployee(
+            @PathVariable UUID employeeId,
+            HttpServletRequest request){
 
         employeeService.deleteByEmployeeId(employeeId);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.ok().body(HttpResponse.builder()
+                .timeStamp(Instant.now())
+                .statusCode(HttpStatus.OK.value())
+                .status(HttpStatus.OK)
+                .message("Employee successfully deleted")
+                .path(request.getRequestURI())
+                .requestMethod(request.getMethod())
+                .build());
+
     }
 
     @GetMapping
     @PreAuthorize("hasAuthority('employee:read')")
-    public ResponseEntity<List<EmployeeAdminResponse>> getEmployees(){
+    public ResponseEntity<HttpResponse> getEmployees(HttpServletRequest request){
 
         List<Employee> employees = employeeService.getEmployees();
 
-        List<EmployeeAdminResponse> response = employees
-                .stream()
-                .map(EmployeeAdminResponse::mapToResponse)
-                .collect(Collectors.toList());
+        return ResponseEntity.ok().body(HttpResponse.builder()
+                .timeStamp(Instant.now())
+                .statusCode(HttpStatus.OK.value())
+                .status(HttpStatus.OK)
+                .message("Employees successfully retrieved")
+                .path(request.getRequestURI())
+                .requestMethod(request.getMethod())
+                .data(Map.of("Employees", employees.stream()
+                        .map(EmployeeAdminResponse::mapToResponse)
+                        .toList()))
+                .build());
 
-        return ResponseEntity.ok(response);
     }
 
 }
