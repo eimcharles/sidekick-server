117c2851b88f0710c635a2a6cce5432bee68d394
Charles Eimer <32907577+eimcharles@users.noreply.github.com>
2026-04-15 17:19:04 -0400

    refactor(employee): split EmployeeService into EmployeeService and EmployeeManagementService.
    - Add validation checks for inactive/deleted employees for EmployeeService.
    - Add validation checks for inactive/deleted employees for EmployeeManagementService.

---
+++ src/main/java/com/eimc/employee/service/EmployeeManagementService.java
@@ -0,0 +1,100 @@
+package com.eimc.employee.service;
+
+import com.eimc.auth.model.ApplicationUser;
+import com.eimc.auth.model.UserRole;
+import com.eimc.common.exception.DuplicateResourceException;
+import com.eimc.common.exception.ResourceNotFoundException;
+import com.eimc.employee.model.Employee;
+import com.eimc.employee.repository.EmployeeRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+@Service
+public class EmployeeManagementService {
+
+    private final EmployeeRepository employeeRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public EmployeeManagementService(EmployeeRepository employeeRepository,
+                                     PasswordEncoder passwordEncoder) {
+        this.employeeRepository = employeeRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public Employee createEmployee(Employee employee,
+                                   String rawPassword,
+                                   UserRole role) {
+
+        if (employeeRepository.existsByEmail(employee.getEmail()))
+            throw new DuplicateResourceException(String.format("Employee with email %s already exists", employee.getEmail()));
+
+        ApplicationUser createdAccount = new ApplicationUser(employee,
+                passwordEncoder.encode(rawPassword),
+                role.getGrantedAuthoritiesAsStrings()
+        );
+
+        employee.setApplicationUser(createdAccount);
+        return employeeRepository.save(employee);
+    }
+
+    public Employee getEmployeeByEmployeeId(UUID employeeId){
+        Employee employee = employeeRepository.findByEmployeeId(employeeId)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("EmployeeId %s not found", employeeId)));
+
+        if (employee.isDeleted()) {
+            throw new ResourceNotFoundException(String.format("EmployeeId %s is inactive", employeeId));
+        }
+
+        return employee;
+    }
+
+
+    @Transactional
+    public Employee updateEmployeeByEmployeeId(UUID employeeId,
+                                               String employeePosition,
+                                               String firstName,
+                                               String lastName) {
+
+        Employee employeeToUpdate = getEmployeeByEmployeeId(employeeId);
+
+        if (employeePosition != null && !employeePosition.isBlank()
+                && !employeePosition.equals(employeeToUpdate.getEmployeePosition())) {
+            employeeToUpdate.setEmployeePosition(employeePosition);
+        }
+
+        if (firstName != null && !firstName.isBlank()
+                && !firstName.equals(employeeToUpdate.getFirstName())) {
+            employeeToUpdate.setFirstName(firstName);
+        }
+
+        if (lastName != null && !lastName.isBlank()
+                && !lastName.equals(employeeToUpdate.getLastName())) {
+            employeeToUpdate.setLastName(lastName);
+        }
+
+        return employeeRepository.save(employeeToUpdate);
+    }
+
+    @Transactional
+    public void deleteByEmployeeId(UUID employeeId) {
+
+        Employee employeeToDelete = getEmployeeByEmployeeId(employeeId);
+
+        employeeToDelete.setIsDeleted(true);
+        employeeToDelete.setDeletedAt(Instant.now());
+        employeeToDelete.getApplicationUser().setIsEnabled(false);
+
+        employeeRepository.save(employeeToDelete);
+    }
+
+    public List<Employee> getEmployees(){
+        return employeeRepository.findAll();
+    }
+
+}
--- src/main/java/com/eimc/employee/service/EmployeeService.java
+++ src/main/java/com/eimc/employee/service/EmployeeService.java
@@ -2,19 +2,14 @@ package com.eimc.employee.service;
 
 import com.eimc.auth.model.ApplicationUser;
 import com.eimc.common.exception.InvalidCurrentPasswordException;
-import com.eimc.common.exception.DuplicateResourceException;
 import com.eimc.common.exception.PasswordMismatchException;
 import com.eimc.common.exception.ResourceNotFoundException;
 import com.eimc.employee.model.Employee;
 import com.eimc.employee.repository.EmployeeRepository;
-import com.eimc.auth.model.UserRole;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Instant;
-import java.util.List;
-import java.util.UUID;
 
 @Service
 public class EmployeeService {
@@ -26,33 +21,6 @@ public class EmployeeService {
                            PasswordEncoder passwordEncoder) {
         this.employeeRepository = employeeRepository;
         this.passwordEncoder = passwordEncoder;
-    }
-
-    @Transactional
-    public Employee createEmployee(Employee employee,
-                                   String rawPassword,
-                                   UserRole role) {
-
-        if (employeeRepository.existsByEmail(employee.getEmail()))
-            throw new DuplicateResourceException(String.format("Employee with email %s already exists", employee.getEmail()));
-
-        ApplicationUser createdAccount = new ApplicationUser(employee,
-                passwordEncoder.encode(rawPassword),
-                role.getGrantedAuthoritiesAsStrings()
-        );
-
-        employee.setApplicationUser(createdAccount);
-        return employeeRepository.save(employee);
-    }
-
-    public Employee getEmployeeByEmployeeId(UUID employeeId){
-        return employeeRepository.findByEmployeeId(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("EmployeeId %s not found", employeeId)));
-    }
-
-    public Employee getEmployeeByEmail(String email) {
-        return employeeRepository.findByEmail(email)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee with %s not found", email)));
     }
 
     @Transactional
@@ -76,48 +44,15 @@ public class EmployeeService {
         employeeRepository.save(employee);
     }
 
-    @Transactional
-    public Employee updateEmployee(UUID employeeId,
-                                   String employeePosition,
-                                   String firstName,
-                                   String lastName) {
+    public Employee getEmployeeByEmail(String email) {
+        Employee employee = employeeRepository.findByEmail(email)
+                .orElseThrow(() -> new ResourceNotFoundException(String.format("Employee with %s not found", email)));
 
-        Employee employeeToUpdate = employeeRepository.findByEmployeeId(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("EmployeeId %s not found", employeeId)));
-
-        if (employeePosition != null && !employeePosition.isBlank()
-                && !employeePosition.equals(employeeToUpdate.getEmployeePosition())) {
-            employeeToUpdate.setEmployeePosition(employeePosition);
+        if (employee.isDeleted()) {
+            throw new ResourceNotFoundException(String.format("Employee with email %s is inactive", email));
         }
 
-        if (firstName != null && !firstName.isBlank()
-                && !firstName.equals(employeeToUpdate.getFirstName())) {
-            employeeToUpdate.setFirstName(firstName);
-        }
-
-        if (lastName != null && !lastName.isBlank()
-                && !lastName.equals(employeeToUpdate.getLastName())) {
-            employeeToUpdate.setLastName(lastName);
-        }
-
-        return employeeRepository.save(employeeToUpdate);
-    }
-
-    @Transactional
-    public void deleteByEmployeeId(UUID employeeId) {
-
-        Employee employeeToDelete = employeeRepository.findByEmployeeId(employeeId)
-                .orElseThrow(() -> new ResourceNotFoundException(String.format("EmployeeId %s not found", employeeId)));
-
-        employeeToDelete.setIsDeleted(true);
-        employeeToDelete.setDeletedAt(Instant.now());
-        employeeToDelete.getApplicationUser().setIsEnabled(false);
-
-        employeeRepository.save(employeeToDelete);
-    }
-
-    public List<Employee> getEmployees(){
-        return employeeRepository.findAll();
+        return employee;
     }
 
 }
